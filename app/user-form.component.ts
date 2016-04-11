8192d2ecf1819b1d55a5d99f4dbbe12eeba01b34
Débora Martins <dehvmartins@gmail.com>
2016-04-11 19:03:23 -0300

    Implements dirty tracking

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,13 +1,14 @@
 import {Component} from 'angular2/core';
 import {ControlGroup,	FormBuilder, Validators} from	'angular2/common';
 import {EmailValidator} from './emailValidator';
+import {CanDeactivate} from 'angular2/router';
 
 @Component({
     selector: 'user-form',
     templateUrl: 'app/user-form.component.html'
 })
 
-export class UserFormComponent{
+export class UserFormComponent implements CanDeactivate {
     userForm: ControlGroup;
 
     constructor(fb: FormBuilder){
@@ -22,6 +23,10 @@ export class UserFormComponent{
             zipcode: []
           })
       });
+    }
+
+    routerCanDeactivate(next, previous){
+        return confirm("Are you sure?");
     }
 
 }
