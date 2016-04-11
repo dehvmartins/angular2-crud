bf5a06468c843ea6ecd12281f33ad5bd48477498
Débora Martins <dehvmartins@gmail.com>
2016-04-11 16:02:52 -0300

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+import {NavBarComponent} from './navbar.component';
+
+@Component({
+    selector: 'my-app',
+    template: '<navbar></navbar>',
+    directives: [NavBarComponent]
+})
+
+export class AppComponent {
+}
