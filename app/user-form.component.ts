7bbb9874d43a8015c5e78789150dbd30d35f9f0b
Débora Martins <dehvmartins@gmail.com>
2016-04-12 08:49:17 -0300

    Adding user

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,17 +1,19 @@
 import {Component} from 'angular2/core';
 import {ControlGroup,	FormBuilder, Validators} from	'angular2/common';
 import {EmailValidator} from './emailValidator';
-import {CanDeactivate} from 'angular2/router';
+import {CanDeactivate, Router} from 'angular2/router';
+import {UsersService} from './users.service';
 
 @Component({
     selector: 'user-form',
-    templateUrl: 'app/user-form.component.html'
+    templateUrl: 'app/user-form.component.html',
+    providers: [UsersService]
 })
 
 export class UserFormComponent implements CanDeactivate {
     userForm: ControlGroup;
 
-    constructor(fb: FormBuilder){
+    constructor(fb: FormBuilder, private _router: Router, private _userService: UsersService){
       this.userForm = fb.group({
           name: ['', Validators.required],
           email: ['', EmailValidator.valid],
@@ -27,6 +29,10 @@ export class UserFormComponent implements CanDeactivate {
 
     routerCanDeactivate(next, previous){
         return confirm("Are you sure?");
+    }
+
+    save(){
+      this._userService.addUser(this.userForm.value).subscribe(x => {this._router.navigate(['Users'])});
     }
 
 }
--- app/users.service.ts
+++ app/users.service.ts
@@ -12,4 +12,8 @@ export class UsersService {
 	getUsers(){
 		return this._http.get(this._url).map(res => res.json());
 	}
+
+	addUser(user){
+		return this._http.post(this._url, JSON.stringify(user)).map(res => res.json());
+	}
 }
